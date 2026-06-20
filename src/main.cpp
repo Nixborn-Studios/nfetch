095feff462f3440bc439820e8ff92b5f6b07ca4c
ray <ytkeyyq@gmail.com>
2026-06-20 13:46:00 +0200

    sgsG

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <chafa.h>
+#ifdef HAVE_CHAFA
+  #include <chafa.h>
+#endif
 #include <globals.hpp>
 #include <utils.hpp>
 
 int main()
 {
+    #ifdef HAVE_CHAFA
     STBImageReturn* nixborn_img = NFETCH::LoadImage(nixborn_logo.c_str());
     NFETCH::PrintNixbornStyle(nixborn_img, 30, 15);
 
     delete nixborn_img;
+    #else
+    std::cout << "This terminal doesn't support nfetch!";
+    #endif
 
     return 0;
 }
